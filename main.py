0e324e0612fc1d4a37592608d7c3ad91a8dbf1ef
takeru.abe <takeru.abe@gaiax.com>
2022-01-22 13:41:57 +0900

    show nmf components

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_lfw_people
+from sklearn.decomposition import NMF
+from sklearn.model_selection import train_test_split
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+people = fetch_lfw_people(min_faces_per_person=20, resize=0.7)
+image_shape = people.images[0].shape
 
+mask = np.zeros(people.target.shape, dtype=bool)
+for target in np.unique(people.target):
+    mask[np.where(people.target == target)[0][:50]] = 1
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+X_people = people.data[mask]
+y_people = people.target[mask]
+X_people = X_people / 255
+X_train, X_test, y_train, y_test = train_test_split(X_people, y_people, stratify=y_people, random_state=0)
 
+nmf = NMF(n_components=15, random_state=0)
+nmf.fit(X_train)
+X_train_nmf = nmf.transform(X_train)
+x_test_nmf = nmf.transform(X_test)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+fig, axes = plt.subplots(3, 5, figsize=(15, 12), subplot_kw={'xticks': (), 'yticks': ()})
+for i, (component, ax) in enumerate(zip(nmf.components_, axes.ravel())):
+    ax.imshow(component.reshape(image_shape))
+    ax.set_title("{}. component".format(i))
+plt.savefig('nmf_componets.png')
